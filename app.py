7d89f5bfb7898acde40427b3a1cdf30646b2ab7e
Daniel Wyleczuk-Stern <dweezy.netsec@gmail.com>
2021-05-20 17:30:48 -0400

    add stocks to not buy

--- app.py
+++ app.py
@@ -13,14 +13,19 @@ def get_stocks_to_buy(data_source, config):
         res = {}
         for ticker in get_tickers("stock_advisor", config):
             res[ticker] = config['amount']
+        if 'custom' in config:
+            for custom_amount in config['custom']:
+                ticker = list(custom_amount.keys())[0]
+                res[ticker] = custom_amount[ticker]
         return res
     elif data_source == "motley_fool_rule_breakers":
         res = {}
         for ticker in get_tickers("rule_breakers", config):
             res[ticker] = config['amount']
-        for custom_amount in config['custom']:
-            ticker = list(custom_amount.keys())[0]
-            res[ticker] = custom_amount[ticker]
+        if 'custom' in config:
+            for custom_amount in config['custom']:
+                ticker = list(custom_amount.keys())[0]
+                res[ticker] = custom_amount[ticker]
         return res
     elif data_source == "google_sheet":
         res = {}
@@ -39,21 +44,36 @@ def get_stocks_to_buy(data_source, config):
     else:
         return {}
 
+def get_stocks_to_not_buy(config):
+    res = []
+    csv_url = config['sheet_url']
+    csv_obj = csv.reader(StringIO(requests.get(url=csv_url).content.decode("utf-8") ), delimiter=',')
+    it = iter(csv_obj)
+    next(it, None)
+    for row in it:
+        res = res + row
+    return res
+
 def main():
     stocks_to_buy = {}
+    stocks_to_not_buy = []
     enviornment = "paper"
     with open('config.yml') as file:
         config_file = yaml.load(file, Loader=yaml.FullLoader)
         environment = config_file['enviornment']
         sources = config_file['sources']
         for source in sources:
-            stocks_to_buy.update(get_stocks_to_buy(source, sources[source]))
+            if source == "do_not_buy_list":
+                stocks_to_not_buy = get_stocks_to_not_buy(sources[source])
+            else:
+                stocks_to_buy.update(get_stocks_to_buy(source, sources[source]))
     for key in stocks_to_buy.keys():
         print(key)
-        try:
-            print(buy_stock(key,stocks_to_buy[key],environment))
-        except Exception as e:
-            print(e)
+        if key not in stocks_to_not_buy:
+            try:
+                print(buy_stock(key,stocks_to_buy[key],environment))
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     main()
